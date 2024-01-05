9be45766150af18d4e66fb1659ae592e8602ca9c
akshataa22 <dhanawadeakshata53@gmail.com>
2024-01-05 22:41:41 +0530

    Welcome message

---
+++ src/com/bl/Payroll_Service.java
@@ -0,0 +1,7 @@
+package com.bl;
+
+public class Payroll_Service {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Payroll Service JDBC connection Program.");
+    }
+}
